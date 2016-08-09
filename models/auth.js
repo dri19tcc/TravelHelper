c4ed1d80e9ca6774d571bf0355c180684bacab14
Adriana <dri19tcc@gmail.com>
2016-08-09 14:03:03 -0700

    Getting OAuth to work

---
+++ models/auth.js
@@ -0,0 +1,21 @@
+var app = require('../app');
+var db = app.get('db');
+
+var User = function (user) {
+  this.id = user.id;
+  this.name = user.name;
+  this.email = user.email;
+  this.image_url = user.image_url
+};
+
+User.find_by_id = function(id, callback) {
+  db.users.findOne({id: id}, function(error, user) {
+    if(error || !user) {
+      callback(new Error("User not found"), undefined)
+    } else {
+      callback(null, new User(user))
+    }
+  })
+}
+
+module.exports = User
--- routes/auth.js
+++ routes/auth.js
@@ -2,17 +2,17 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport'); // For google oauth
 
-router.get('/google',
+router.get('/google_oauth2',
   passport.authenticate('google', {
     scope: ['profile']
   }));
 
-router.get('/google/callback',
+router.get('/google_oauth2/callback',
   passport.authenticate('google', {
-    failureRedirect: '/auth'
+    failureRedirect: '/'
   }), function(req, res) {
     // Successful authentication, redirect home.
-  res.redirect('/auth');
+  res.redirect('/');
 });
 
 module.exports = router;
