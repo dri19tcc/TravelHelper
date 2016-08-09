2e21f1e82823145efec3acd4dbfdb0af1a4a4722
Adriana <dri19tcc@gmail.com>
2016-08-08 21:26:10 -0700

    Added some tables to my schema

--- db/setup/schema.sql
+++ db/setup/schema.sql
@@ -2,4 +2,19 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users(
   id serial PRIMARY KEY,
   name text,
+  email text,
+  image_url text
+);
+
+DROP TABLE IF EXISTS tag;
+CREATE TABLE tag(
+  id serial PRIMARY KEY,
+  name text,
+  modified_date date,
+);
+
+DROP TABLE IF EXISTS activity;
+CREATE TABLE activity(
+  id serial PRIMARY KEY,
+  -- More will go here, still deciding what
 );
