5e427e5c15cc7f6996a15698305018cd9d2125fe
Adriana <dri19tcc@gmail.com>
2016-08-08 21:29:26 -0700

    Added in my joining tables into my database

--- db/setup/schema.sql
+++ db/setup/schema.sql
@@ -10,11 +10,25 @@ DROP TABLE IF EXISTS tag;
 CREATE TABLE tag(
   id serial PRIMARY KEY,
   name text,
-  modified_date date,
+  modified_date date
 );
 
 DROP TABLE IF EXISTS activity;
 CREATE TABLE activity(
-  id serial PRIMARY KEY,
+  id serial PRIMARY KEY
   -- More will go here, still deciding what
+);
+
+DROP TABLE IF EXISTS user_tag;
+CREATE TABLE activity(
+  id serial PRIMARY KEY,
+  user_id int,
+  tag_id int
+);
+
+DROP TABLE IF EXISTS activity_tag;
+CREATE TABLE activity(
+  id serial PRIMARY KEY,
+  activity_id int,
+  tag_id int,
 );
