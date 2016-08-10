d73bb6957a56360f62edb107eebee269819fbadd
Adriana <dri19tcc@gmail.com>
2016-08-10 10:38:56 -0700

    Working on session data saving

--- controllers/index.js
+++ controllers/index.js
@@ -17,3 +17,16 @@ IndexController = {
 }
 
 module.exports = IndexController
+
+// def create
+//     auth_hash = request.env['omniauth.auth']
+//     user = User.find_or_create_from_omniauth(auth_hash)
+//
+//     if user
+//       session[:user_id] = user.id
+//       redirect_to users_path
+//     else
+//       flash[:notice] = "Nope"
+//       redirect_to root_path
+//     end
+//   end
