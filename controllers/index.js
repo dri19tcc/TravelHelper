f3a05ad450e5b9e04955da7437ae2555b81bc646
Adriana <dri19tcc@gmail.com>
2016-08-10 15:02:09 -0700

    Cleaning up code as well as adding to the index page for when a user is signed in

--- controllers/index.js
+++ controllers/index.js
@@ -4,24 +4,21 @@ IndexController = {
 
   getIndex: function(req, res) {
     if (req.session.passport) { // if req.session.passport.id exists then user is currently signed in
-      console.log("hello, first thing");
       var user = req.session.passport.user;
       Index.findOrCreate(user, function(error, result, next) {
         if (error) {
-          console.log("error: ", error);
             next(error);
         } else {
           var loggedIn = req.session.passport ? true : false;
-          console.log("THIS IS THE RESULT: ", result);
           res.render('index', {
             title: "Travel Helper",
             loggedIn: loggedIn,  // This is for the sign in/logout feature
-            username: result.name
+            username: result.name,
+            image: result.image_url
           })
         }
       })
     } else {
-      // console.log(req.session.passport);
       var loggedIn = req.session.passport ? true : false;
       res.render('index', {
         title: "Travel Helper",
