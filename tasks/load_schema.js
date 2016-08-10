f9c66405fe5aab7c370033b1908a883a5031c3fa
Adriana <dri19tcc@gmail.com>
2016-08-10 13:22:36 -0700

    Did not handle errors in schema

--- tasks/load_schema.js
+++ tasks/load_schema.js
@@ -1,12 +1,12 @@
-var massive = require('massive')
-var connectionString = "postgres://localhost/travel_helper"
+var massive = require('massive');
+var connectionString = "postgres://localhost/travel_helper";
 
-var db = massive.connectSync({connectionString : connectionString})
+var db = massive.connectSync({connectionString : connectionString});
 
 db.setup.schema([], function(err, res) {
-  if(err) {
-    throw(new Error(err.message))
-  }
-  console.log("yay schema!")
-  process.exit()
-})
+  // if(err) {
+  //   throw(new Error(err.message))
+  // }
+  console.log("yay schema!");
+  process.exit();
+});
